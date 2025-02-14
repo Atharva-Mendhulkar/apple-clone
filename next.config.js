e2df35bb6e40e13e53cde66f2c5cb4f4023b5106
Atharva-mendhu <your.email@example.com>
2025-02-15 00:44:30 +0530

    Vercel fix 2

--- next.config.js
+++ next.config.js
@@ -1,12 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    unoptimized: true,
+  },
   webpack: (config) => {
     config.module.rules.push({
       test: /\.(glb|gltf)$/,
-      type: 'asset/resource'
+      use: {
+        loader: 'file-loader',
+        options: {
+          publicPath: '/_next/static/media',
+          outputPath: 'static/media',
+          name: '[name].[hash].[ext]',
+        },
+      },
     });
     return config;
-  }
+  },
+  // Ensure static files are handled correctly
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/_next' : ''
 }
 
 export default nextConfig;
